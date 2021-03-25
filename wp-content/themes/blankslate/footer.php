eda8163a9408b5a00bacac13d84836fd90547209
Grant Emerson <grant.alex.emerson@gmail.com>
2021-03-25 10:42:35 -0500

    main page complete

--- wp-content/themes/blankslate/footer.php
+++ wp-content/themes/blankslate/footer.php
@@ -1,7 +1,10 @@
 </div>
 <footer id="footer">
 <div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+<!-- &copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?> -->
+<img class="site-logo2" src="<?php echo get_theme_file_uri('images/s&s_logo_white.png')?>"/>
+<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'menu_class' => 'menu-2' ) ); ?>
+
 </div>
 </footer>
 </div>
