854defbc68e2e73e9ad362f3aad91e28e9f74347
Grant Emerson <grant.alex.emerson@gmail.com>
2021-03-29 11:31:02 -0500

    added color overlay final previewing before sending off

--- wp-content/themes/blankslate/index.php
+++ wp-content/themes/blankslate/index.php
@@ -1,15 +1,18 @@
 <?php get_header(); ?>
 <main id="content">
-<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/GettyImages-526245919.jpg')?>);"></div>
-      <div class="colorOverlay"></div>
-<div class="heroContentContainer">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/CarCrash.jpg')?>);"></div>
+        <div class="coverOverlayContainer">
+            <div class="colorOverlay"></div>
+        </div>      
+
+    <div class="heroContentContainer">
           <h1 class="heroText">Lorem Ipsum Dolor Sit Amet</h1>
           <button class="btn">Contact us</button>
-      </div>
+    </div>
       <div class="blueBanner">
-    <h2 class="bannerTest">Smith & Smith Will Fight For You!</h2>
+         <h2 class="bannerTest">Smith & Smith Will Fight For You!</h2>
     
-</div>
+        </div>
     <div class="personalInjuryContainer">
         <h2 class="personalInjuryTitleText"><span class="graphicDesignLines">_____&nbsp&nbsp</span>Personal Injury Lawyers<span class="graphicDesignLines"> &nbsp&nbsp_____</span></h2>
         <div class="slipAndFallContainer">
